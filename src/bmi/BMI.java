bb8528c0d17a4009e8cbcd9baa5fdb9059564bed
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-14 18:01:19 +0500

    BMI IN JAVA

---
+++ src/bmi/BMI.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bmi;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+import java.math.*;
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args) {
+        // TODO code application logic here
+//    Scanner obj=new Scanner (System.in);
+//        System.out.print("Enter weight:");
+//    double weight=obj.nextDouble();
+//    double wik = weight*(0.45359237);
+//        System.out.print("Enter inch:");
+//    double inch=obj.nextDouble();
+//    double hif= inch*(0.0254);
+//    double bmi=(wik/hif)*(wik/hif);
+//        System.out.println(bmi);
+//    
+    
+    
+    int a=5,b=2,c=3;
+    
+//        System.out.println(Math.max(a,b,));
+        if (a > b){
+        System.out.println("a is greater");
+            if(a> c){
+                System.out.println("a is greater");
+            }
+        }
+        else if(b> a){
+        
+        }
+        
+        
+        
+        
+        
+    
+    }
+    
+}
